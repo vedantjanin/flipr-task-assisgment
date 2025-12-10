6391f105a44594bcfe76b9da474e72ea39257353
Vedant jain <148189535+vedantjanin@users.noreply.github.com>
2025-12-10 14:47:47 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,12 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
